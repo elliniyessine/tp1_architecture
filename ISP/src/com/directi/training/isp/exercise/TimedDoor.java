2d912abe2aecf3c0655896c0dbfe5e5978dfdfd4
GH1hatem <gharsallahhatem58@gmail.com>
2025-02-28 00:03:59 +0100

    Refactor the ISP excercice

--- ISP/src/com/directi/training/isp/exercise/TimedDoor.java
+++ ISP/src/com/directi/training/isp/exercise/TimedDoor.java
@@ -1,53 +1,22 @@
 package com.directi.training.isp.exercise;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-public class TimedDoor implements Door
-{
+public class TimedDoor implements Door, TimerClient {
     private static final int TIME_OUT = 100;
     private boolean _locked;
     private boolean _opened;
 
-    public TimedDoor(Timer timer)
-    {
-        timer.register(TIME_OUT, this);
+    public TimedDoor(Timer timer) {
+        timer.register(TIME_OUT, this); // Now registers as TimerClient
     }
 
-    @Override
-    public void lock()
-    {
-        _locked = true;
-    }
+    @Override public void lock() { _locked = true; }
+    @Override public void unlock() { _locked = false; }
+    @Override public void open() { if (!_locked) _opened = true; }
+    @Override public void close() { _opened = false; }
 
     @Override
-    public void unlock()
-    {
-        _locked = false;
-    }
-
-    @Override
-    public void open()
-    {
-        if (!_locked) {
-            _opened = true;
-        }
-    }
-
-    @Override
-    public void close()
-    {
-        _opened = false;
-    }
-
-    @Override
-    public void timeOutCallback()
-    {
-        _locked = true;
-    }
-
-    @Override
-    public void proximityCallback()
-    {
-        throw new NotImplementedException();
+    public void timeOutCallback() {
+        _locked = true; // No more NotImplementedException!
     }
 }
